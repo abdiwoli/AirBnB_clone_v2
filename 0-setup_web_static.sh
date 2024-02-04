68cef470373fe53b4d944172c922f6cf39ac490e
abdiwoli <abdiwolih.gmail.com>
2024-02-04 01:37:49 +0000

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,5 +11,5 @@ sudo echo "abdiwoli page " | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '/^server {/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
-
+sudo ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled
 sudo systemctl restart nginx
