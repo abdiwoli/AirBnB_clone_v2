7564122cc849a6af2692c6ed074e88e0cee6ff3a
abdiwoli <abdiwolih.gmail.com>
2024-02-04 16:54:20 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y nginx > /dev/null 2>&1
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo "abdiwoli page " | sudo tee /data/web_static/releases/test/index.html
+sudo echo "abdiwoli page " | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '/^server {/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
