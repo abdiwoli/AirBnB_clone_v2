a9625b794c58cfd108e6a64cf0721718b8483bc5
abdiwoli <abdiwolih@gmail.com>
2024-02-18 19:07:39 +0300

    done

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,13 +9,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """ close sesion """
-    print("Teardown app context called.")
-    storage.close()
-
-
 @app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
 def states(id=None):
@@ -32,5 +25,12 @@ def states(id=None):
         return render_template('9-states.html')
 
 
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """ close sesion """
+    print("Teardown app context called.")
+    storage.close()
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
