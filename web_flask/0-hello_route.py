0ca96118ae740ed1a013d6b4285bd955b84c1b8e
abdiwoli <abdiwolih@gmail.com>
2024-02-19 04:37:30 +0300

    fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,11 +5,13 @@ from markupsafe import escape
 
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     """ main page """
     return "Hello HBNB!"
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
